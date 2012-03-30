7a674c46598acad20efd24c265a87e8608f3d556
Shingo <ninjashingo@gmail.com>
2012-03-29 21:50:57 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Statsfaq::Application.config.secret_token = '0c97ecf8f9748e1b82b8eac5c7104888ae8c8e2b740417aafdb6cedcbdd497d171325da913696a383145ec29bee064d7c775303273ed5c60e6dd3b93b9f89359'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Statsfaq::Application.config.session_store :cookie_store, key: '_statsfaq_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Statsfaq::Application.config.session_store :active_record_store
